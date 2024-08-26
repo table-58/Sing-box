fc932ef6ccb8944e6e9700d8fd3ca09d6548950f
table-58 <table.dc2712@gmail.com>
2024-08-26 12:25:20 +0800

    Update sb_00.sh

--- sb_00.sh
+++ sb_00.sh
@@ -14,10 +14,10 @@ reading() { read -p "$(red "$1")" "$2"; }
 
 USERNAME=$(whoami)
 HOSTNAME=$(hostname)
-export UUID=${UUID:-'bc97f674-c578-4940-9234-0a1da46041b9'}
-export NEZHA_SERVER=${NEZHA_SERVER:-''} 
-export NEZHA_PORT=${NEZHA_PORT:-'5555'}     
-export NEZHA_KEY=${NEZHA_KEY:-''}
+export UUID=${UUID:-'74a56148-f45e-414c-9afa-44f5ffefb069'}
+export NEZHA_SERVER=${NEZHA_SERVER:-'nz.saggrw.filegear-sg.me'} 
+export NEZHA_PORT=${NEZHA_PORT:-'3042'}     
+export NEZHA_KEY=${NEZHA_KEY:-'NwCWbsGcggbtunLJ8J'}
 
 [[ "$HOSTNAME" == "s1.ct8.pl" ]] && WORKDIR="domains/${USERNAME}.ct8.pl/logs" || WORKDIR="domains/${USERNAME}.serv00.net/logs"
 [ -d "$WORKDIR" ] || (mkdir -p "$WORKDIR" && chmod 777 "$WORKDIR")
